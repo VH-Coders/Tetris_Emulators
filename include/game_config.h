b2c3943ba149f7c0d2f63caddd34b952e0d69a10
Zane <zanehargis@gmail.com>
2023-09-15 11:08:35 -0700

    feat: implement basic game loop
    This sets up the bare minimum boilerplate raylib code need to run the
    game.

---
+++ include/game_config.h
@@ -0,0 +1,16 @@
+/* game_config.h
+ *
+ * This header file contains definitions for basic game and window settings.
+ * It's intended to be able to be included in any module.
+ *
+ */
+#ifndef TETRIS_GAME_CONFIG_H
+#define TETRIS_GAME_CONFIG_H
+
+#define WINDOW_TITLE "Tetris"
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 600
+
+#define GAME_FPS 60
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../lib/raylib/src/raylib.h"
+#include "../include/game_config.h"
 
-int main() {
-    printf("Hello World\n");
+int main() 
+{
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+    SetTargetFPS(GAME_FPS);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
